6291c50d6c443e33d2d9fec8d2a2834c37a4270b
AyushBa97 <ayush.bandopadhyay@gmail.com>
2022-07-16 19:11:03 +0530

    update new Git changes

--- MathVar.py
+++ MathVar.py
@@ -1,24 +1,15 @@
 import math
 
-
-print(2+5)
-print(5-2)
-print(2*2)
-print(2/5)
-print(5//2)
-print(5%2)
-print(2**7)
+print(2 + 5)
+print(5 - 2)
+print(2 * 2)
+print(2 / 5)
+print(5 // 2)
+print(5 % 2)
+print(2 ** 7)
 
 print(math.log(14))
+print(math.pow(7, 5))
 print()
 print()
 print()
-print()
-
-
-
-
-
-
-
-
